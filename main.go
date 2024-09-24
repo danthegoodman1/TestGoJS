2b57bf2bd70763f47213a7d6b7b8561939ad6bb0
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-09-24 11:59:53 -0700

    profile

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/dop251/goja"
+	"github.com/google/pprof/profile"
 	"github.com/robertkrimen/otto"
 	"time"
 )
 
 func main() {
-	gojaJSClass()
+	// gojaJSClass()
+	profiled()
 }
 
 func creation() {
@@ -38,6 +41,42 @@ while (i++ < 1e6) {
 	}
 	fmt.Println("Goja in", time.Now().Sub(s))
 
+}
+
+func profiled() {
+	js := `function factorial(n) {
+    return n === 1 ? n : n * factorial(--n);
+}
+
+var i = 0;
+
+while (i++ < 100000) {
+    factorial(10);
+}`
+
+	b := &bytes.Buffer{}
+
+	err := goja.StartProfile(b)
+	if err != nil {
+		panic(err)
+	}
+
+	gm := goja.New()
+	s := time.Now()
+	_, err = gm.RunString(js)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Goja in", time.Now().Sub(s))
+
+	goja.StopProfile()
+
+	// read the profile
+	p, err := profile.Parse(bytes.NewReader(b.Bytes()))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(p)
 }
 
 func factorial() {
